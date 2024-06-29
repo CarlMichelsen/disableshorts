004ec7a92c4650181be7e23650a58f111bcc6ecf
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2024-06-29 10:44:49 +0200

    More robust shorts disabling

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url) {
         const url = new URL(tab.url);
-        if (url.hostname === 'www.youtube.com' && url.pathname === '/shorts') {
+        if (url.hostname === 'www.youtube.com') {
             browser.tabs.executeScript(tabId, { file: 'content_script.js' });
         }
     }
--- src/content_script.ts
+++ src/content_script.ts
@@ -1,26 +1,65 @@
-const onShorts = (currentPathname: string, lastPathname?: string) => {
-    if (!currentPathname.startsWith("/shorts")) {
-        return;
+class ShortsDenier {
+    private static instance: ShortsDenier;
+    private observing: boolean;
+    private timeout: number | null;
+    private lastPathName: string | null;
+    private observer: MutationObserver;
+
+    private constructor() {
+        this.observing = false;
+        this.timeout = null;
+        this.lastPathName = null;
+        this.observationHandler = this.observationHandler.bind(this);
+        this.observer = new MutationObserver(this.observationHandler);
     }
 
-    console.log("No shorts for you!");
-    if (lastPathname && !lastPathname.startsWith("/shorts")) {
-        window.history.back();
-    } else {
-        const rootUrl = `${window.location.protocol}//${window.location.host}/`;
-        window.location.href = rootUrl;
+    public static getInstance(): ShortsDenier {
+        if (!ShortsDenier.instance) {
+            ShortsDenier.instance = new ShortsDenier();
+        }
+        return ShortsDenier.instance;
+    }
+
+    private queueCheckShorts(currentPathname: string) {
+        if (this.timeout !== null) {
+            clearTimeout(this.timeout);
+        }
+    
+        this.timeout = setTimeout(() => {
+            this.checkShorts(currentPathname);
+            this.timeout = null;
+        }, 200);
+    }
+
+    private checkShorts(currentPathname: string) {
+        if (!currentPathname.startsWith("/shorts")) {
+            return;
+        }
+    
+        console.log("No YouTube Shorts for you!");
+        if (this.lastPathName && !this.lastPathName.startsWith("/shorts")) {
+            window.history.back();
+        } else {
+            const rootUrl = `${window.location.protocol}//${window.location.host}/`;
+            window.location.href = rootUrl;
+        }
+    }
+
+    private observationHandler() {
+        const currentPathname = window.location.pathname;
+        if (currentPathname !== this.lastPathName) {
+            this.queueCheckShorts(currentPathname);
+            this.lastPathName = currentPathname;
+        }
+    }
+
+    public observe(): void {
+        if (!this.observing) {
+            this.observer.observe(document, { childList: true, subtree: true });
+            this.observing = true;
+        }
     }
 }
 
-onShorts(window.location.pathname);
 
-let lastPathname = window.location.pathname;
-const observer = new MutationObserver(() => {
-    const currentPathname = window.location.pathname;
-    if (currentPathname !== lastPathname) {
-        onShorts(currentPathname, lastPathname);
-        lastPathname = currentPathname;
-    }
-});
-
-observer.observe(document, { childList: true, subtree: true });
+ShortsDenier.getInstance().observe();
